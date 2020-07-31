027243d20d6bb4f4a8876b75ebe2731baa579de4
Micheal Hansen <micheal.hansen@gmail.com>
2020-07-31 15:10:21 -0700

    add details method to appointments controller

--- HairSalon/Controllers/AppointmentsController.cs
+++ HairSalon/Controllers/AppointmentsController.cs
@@ -20,5 +20,13 @@ namespace HairSalon.Controllers
       IEnumerable<Appointment> model = _db.Appointments.Include(apps => apps.Stylist).Include(apps => apps.Client).ToList().OrderBy(apps => apps.Stylist.Name).ThenBy(apps => apps.Time);
       return View(model);
     }
+
+    public ActionResult Details(int id)
+    {
+      Appointment appointment = _db.Appointments.Include(apps => apps.Stylist).Include(apps => apps.Client).FirstOrDefault(apps => apps.AppointmentId == id);
+      return View(appointment);
+    }
+
+    
   }
 }
