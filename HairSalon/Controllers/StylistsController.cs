49654ccf7b9ffdb2efd7e4f28f84235d15b21c51
Micheal Hansen <micheal.hansen@gmail.com>
2020-07-31 09:19:17 -0700

    add edit method to stylists

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -40,6 +40,18 @@ namespace HairSalon.Controllers
       return View(stylist);
     }
 
-    
+    public ActionResult Edit(int id)
+    {
+      Stylist stylist = _db.Stylists.FirstOrDefault(stylists => stylists.StylistId == id);
+      return View(stylist);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Stylist stylist)
+    {
+      _db.Entry(stylist).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = stylist.StylistId });
+    }
   }
 }
