a8567dacc10631934b51c959508e96704b1395f7
Micheal Hansen <micheal.hansen@gmail.com>
2020-07-31 08:53:40 -0700

    update project to include dbcontext for entity integration

--- HairSalon/Startup.cs
+++ HairSalon/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using ProjectName.Models;
+using HairSalon.Models;
 
 namespace HairSalon
 {
@@ -25,7 +25,7 @@ namespace HairSalon
       services.AddMvc();
 
       services.AddEntityFrameworkMySql()
-        .AddDBContext<ProjectNameContext>(options => options
+        .AddDBContext<HairSalonContext>(options => options
         .UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
     }
 
