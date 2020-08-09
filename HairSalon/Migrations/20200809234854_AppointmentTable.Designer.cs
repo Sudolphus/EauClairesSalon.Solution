80519b203bd6b2b6922ed486898fccd8207ebb93
Micheal Hansen <micheal.hansen@gmail.com>
2020-08-09 16:49:27 -0700

    update migrations to include appointment table

--- HairSalon/Migrations/20200809234854_AppointmentTable.Designer.cs
+++ HairSalon/Migrations/20200809234854_AppointmentTable.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using HairSalon.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HairSalon.Migrations
 {
     [DbContext(typeof(HairSalonContext))]
-    partial class HairSalonContextModelSnapshot : ModelSnapshot
+    [Migration("20200809234854_AppointmentTable")]
+    partial class AppointmentTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ HairSalon/Migrations/20200809234854_AppointmentTable.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HairSalon.Migrations
+{
+    public partial class AppointmentTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Appointments",
+                columns: table => new
+                {
+                    AppointmentId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DayTime = table.Column<DateTime>(nullable: false),
+                    StylistId = table.Column<int>(nullable: false),
+                    ClientId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointments", x => x.AppointmentId);
+                    table.ForeignKey(
+                        name: "FK_Appointments_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "ClientId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Appointments_Stylists_StylistId",
+                        column: x => x.StylistId,
+                        principalTable: "Stylists",
+                        principalColumn: "StylistId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_ClientId",
+                table: "Appointments",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_StylistId",
+                table: "Appointments",
+                column: "StylistId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Appointments");
+        }
+    }
+}
